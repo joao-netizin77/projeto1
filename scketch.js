4976cae0326a4ff6b02702879c43deb50712a94b
joao-netizin77 <0000111222354xsp@al.educacao.sp.gov.br>
2024-08-05 17:02:50 -0300

    Create scketch.js

---
+++ scketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 1000);
+  background("rgb(10,10,100)");
+}
+
+function draw() {
+  
+  fill("rgb(10,10,11)");
+  stroke("rgb(122,8,8)");
+  if(mouseIsPressed)
+  
+  
+  circle(mouseX, mouseY, 20, 30);
+}
